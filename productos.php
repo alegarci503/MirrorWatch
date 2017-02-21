02d2147a8a730151d591e50eb1c303523553e421
alegarci503 <alejandro.portillo8@gmail.com>
2017-02-20 20:39:54 -0600

    Esta es la base de la pagina

---
+++ productos.php
@@ -0,0 +1,280 @@
+<!DOCTYPE html>
+  <html lang="ES">
+    <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>MirrorWatch</title>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+      <link type="text/css" rel="stylesheet" href="css/miestilo.css"  media="screen,projection"/>
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+    <div class="navbar-fixed">
+    <nav>
+        <div class="nav-wrapper grey lighten-4">
+        <a href="#!" class="brand-logo"><img class="pequeña" src="img/Original.png"></a>
+        <ul class="right hide-on-med-and-down">
+            <li><a href="index.php" class="black-text ">Inicio<i class="material-icons left">home</i></a></li>
+            <li><a href="index.php" class="black-text ">Login<i class="material-icons left">person</i></a></li>
+            <li><a href="productos.php" class="black-text ">Productos<i class="material-icons left">watch</i></i></a></li>
+            <li><a href="soporte.php" class="black-text ">Cuenta<i class="material-icons left">portrait</i></a></li>
+            <li><a href="nosotros.php" class="black-text ">Nosotros<i class="material-icons left">business</i></a></li>
+            <li><a href="carrito.php" class="black-text "><i class="material-icons">local_grocery_store</i></a></li>
+            <li><a href="soporte.php" class="black-text "><i class="material-icons">build</i></a></li>
+        </ul>
+        </div>
+    </nav>
+    </div>
+    <body style="background-color:#e0e0e0;">
+    <div class="parallax-container">
+      <div class="parallax"><img class="mediana" src="img/parallax.jpg"></div>
+    </div>
+        <div class="container">
+        <div class="row">
+        <div class="col s4 m4">
+        <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator grande" src="img/sony2.jpeg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Sony SW 3<i class="material-icons right">more_vert</i></span>
+      <p><a href="#">Comprar<i class="material-icons left">shopping_cart</i></a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4"><Especificaciones></Especificaciones><i class="material-icons right">close</i></span>
+      <div class="divider"></div>
+  <div class="section">
+    <h5>Pantalla</h5>
+    <p>LCD de 1,6 pulgadas con resolucion de 320 x 320 píxeles</p>
+  </div>
+  <div class="divider"></div>
+  <div class="section">
+    <h5>Procesador</h5>
+    <p>Qualcomm Snapdragon 400 a 1,2 GHz</p>
+  </div>
+  <div class="divider"></div>
+  <div class="section">
+    <h5>Almacenamiento</h5>
+    <p>4GB internos y 512MB de RAM</p>
+  </div>
+  <div class="divider"></div>
+  <div class="section">
+    <h5>Bateria</h5>
+    <p>	420 mAh</p>
+  </div>
+    </div>
+  </div>
+  </div>
+  <div class="col s4 m4">
+        <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator grande" src="img/moto2.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Moto 360<i class="material-icons right">more_vert</i></span>
+      <p><a href="#">Comprar<i class="material-icons left">shopping_cart</i></a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4"><Especificaciones></Especificaciones><i class="material-icons right">close</i></span>
+      <div class="divider"></div>
+  <div class="section">
+    <h5>Pantalla</h5>
+    <p>LCD de 1,6 pulgadas con resolucion de 320 x 320 píxeles</p>
+  </div>
+  <div class="divider"></div>
+  <div class="section">
+    <h5>Procesador</h5>
+    <p>Qualcomm Snapdragon 400 a 1,2 GHz</p>
+  </div>
+  <div class="divider"></div>
+  <div class="section">
+    <h5>Almacenamiento</h5>
+    <p>4GB internos y 512MB de RAM</p>
+  </div>
+  <div class="divider"></div>
+  <div class="section">
+    <h5>Bateria</h5>
+    <p>	420 mAh</p>
+  </div>
+    </div>
+  </div>
+  </div>
+  <div class="col s4 m4">
+        <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator grande" src="img/samsung2.png">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Samsung Gear S2<i class="material-icons right">more_vert</i></span>
+      <p><a href="#">Comprar<i class="material-icons left">shopping_cart</i></a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4"><Especificaciones></Especificaciones><i class="material-icons right">close</i></span>
+      <div class="divider"></div>
+  <div class="section">
+    <h5>Pantalla</h5>
+    <p>LCD de 1,6 pulgadas con resolucion de 320 x 320 píxeles</p>
+  </div>
+  <div class="divider"></div>
+  <div class="section">
+    <h5>Procesador</h5>
+    <p>Qualcomm Snapdragon 400 a 1,2 GHz</p>
+  </div>
+  <div class="divider"></div>
+  <div class="section">
+    <h5>Almacenamiento</h5>
+    <p>4GB internos y 512MB de RAM</p>
+  </div>
+  <div class="divider"></div>
+  <div class="section">
+    <h5>Bateria</h5>
+    <p>	420 mAh</p>
+  </div>
+    </div>
+  </div>
+  </div>  
+  </div>
+ </div>
+ <div class="parallax-container">
+      <div class="parallax"><img class= "mediana" src="img/parallax1.jpg"></div>
+    </div>
+<div class="container">
+        <div class="row">
+        <div class="col s4 m4">
+        <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator correcto" src="img/rolex.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">COSMOGRAPH<i class="material-icons right">more_vert</i></span>
+      <p><a href="#">Comprar<i class="material-icons left">shopping_cart</i></a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4"><Especificaciones></Especificaciones><i class="material-icons right">close</i></span>
+      <div class="divider"></div>
+  <div class="section">
+    <h5>Pantalla</h5>
+    <p>LCD de 1,6 pulgadas con resolucion de 320 x 320 píxeles</p>
+  </div>
+  <div class="divider"></div>
+  <div class="section">
+    <h5>Procesador</h5>
+    <p>Qualcomm Snapdragon 400 a 1,2 GHz</p>
+  </div>
+  <div class="divider"></div>
+  <div class="section">
+    <h5>Almacenamiento</h5>
+    <p>4GB internos y 512MB de RAM</p>
+  </div>
+  <div class="divider"></div>
+  <div class="section">
+    <h5>Bateria</h5>
+    <p>	420 mAh</p>
+  </div>
+    </div>
+  </div>
+  </div>
+  <div class="col s4 m4">
+        <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator correcto" src="img/rolex2.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">PEARLMASTER 39<i class="material-icons right">more_vert</i></span>
+      <p><a href="#">Comprar<i class="material-icons left">shopping_cart</i></a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4"><Especificaciones></Especificaciones><i class="material-icons right">close</i></span>
+      <div class="divider"></div>
+  <div class="section">
+    <h5>Pantalla</h5>
+    <p>LCD de 1,6 pulgadas con resolucion de 320 x 320 píxeles</p>
+  </div>
+  <div class="divider"></div>
+  <div class="section">
+    <h5>Procesador</h5>
+    <p>Qualcomm Snapdragon 400 a 1,2 GHz</p>
+  </div>
+  <div class="divider"></div>
+  <div class="section">
+    <h5>Almacenamiento</h5>
+    <p>4GB internos y 512MB de RAM</p>
+  </div>
+  <div class="divider"></div>
+  <div class="section">
+    <h5>Bateria</h5>
+    <p>	420 mAh</p>
+  </div>
+    </div>
+  </div>
+  </div>
+  <div class="col s4 m4">
+        <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator correcto" src="img/rolex3.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">EXPLORER II<i class="material-icons right">more_vert</i></span>
+      <p><a href="#">Comprar<i class="material-icons left">shopping_cart</i></a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4"><Especificaciones></Especificaciones><i class="material-icons right">close</i></span>
+      <div class="divider"></div>
+  <div class="section">
+    <h5>Pantalla</h5>
+    <p>LCD de 1,6 pulgadas con resolucion de 320 x 320 píxeles</p>
+  </div>
+  <div class="divider"></div>
+  <div class="section">
+    <h5>Procesador</h5>
+    <p>Qualcomm Snapdragon 400 a 1,2 GHz</p>
+  </div>
+  <div class="divider"></div>
+  <div class="section">
+    <h5>Almacenamiento</h5>
+    <p>4GB internos y 512MB de RAM</p>
+  </div>
+  <div class="divider"></div>
+  <div class="section">
+    <h5>Bateria</h5>
+    <p>	420 mAh</p>
+  </div>
+    </div>
+  </div>
+  </div>  
+  </div>
+ </div>
+    <footer class="page-footer  grey darken-2">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Footer Content</h5>
+                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright grey darken-4">
+            <div class="container">
+            Todos los derechos reservados © 2014 Copyright MirrorWatch
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+          </div>
+        </footer>
+      <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+      <script type="text/javascript" src="js/inicia.js"></script>
+    </body>
+  </html>
+        
