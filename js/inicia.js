41c97efe140144215f0fd918b146e2d146d25dae
ByronSolorzano <basfuentes12@hotmail.com>
2017-02-22 17:21:52 -0600

    Subir

--- js/inicia.js
+++ js/inicia.js
@@ -1,16 +1,9 @@
 $(document).ready(function(){
       $('.parallax').parallax();
-<<<<<<< HEAD
-
-        $(".button-collapse").sideNav();
-    
-        $('ul.tabs').tabs();
-=======
     });
 
     $('.carousel.carousel-slider').carousel({fullWidth: true});
 
     $(document).ready(function(){
       $('.slider').slider();
->>>>>>> master
     });
---
+++ productos.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+  <html lang="ES">
+    <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>MirrorWatch</title>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+      <link type="text/css" rel="stylesheet" href="css/miestilo.css"  media="screen,projection"/>
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>   
+
+    <body style="background-color:#e0e0e0;">
+
+      <?php
+
+      include("inc/Menu.php");
+
+      ?>
+
+
+    <div class="parallax-container">
+      <div class="parallax"><img class="mediana" src="img/parallax.jpg"></div>
+    </div>
+        <div class="container">
+        <div class="row">
+        <div class="col s4 m4">
+        <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator grande" src="img/sony2.jpeg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Sony SW 3<i class="material-icons right">more_vert</i></span>
+      <p><a href="#">Comprar<i class="material-icons left">shopping_cart</i></a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4"><Especificaciones></Especificaciones><i class="material-icons right">close</i></span>
+      <div class="divider"></div>
+  <div class="section">
+    <h5>Pantalla</h5>
+    <p>LCD de 1,6 pulgadas con resolucion de 320 x 320 píxeles</p>
+  </div>
+  <div class="divider"></div>
+  <div class="section">
+    <h5>Procesador</h5>
+    <p>Qualcomm Snapdragon 400 a 1,2 GHz</p>
+  </div>
+  <div class="divider"></div>
+  <div class="section">
+    <h5>Almacenamiento</h5>
+    <p>4GB internos y 512MB de RAM</p>
+  </div>
+  <div class="divider"></div>
+  <div class="section">
+    <h5>Bateria</h5>
+    <p>	420 mAh</p>
+  </div>
+    </div>
+  </div>
+  </div>
+  <div class="col s4 m4">
+        <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator grande" src="img/moto2.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Moto 360<i class="material-icons right">more_vert</i></span>
+      <p><a href="#">Comprar<i class="material-icons left">shopping_cart</i></a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4"><Especificaciones></Especificaciones><i class="material-icons right">close</i></span>
+      <div class="divider"></div>
+  <div class="section">
+    <h5>Pantalla</h5>
+    <p>LCD de 1,6 pulgadas con resolucion de 320 x 320 píxeles</p>
+  </div>
+  <div class="divider"></div>
+  <div class="section">
+    <h5>Procesador</h5>
+    <p>Qualcomm Snapdragon 400 a 1,2 GHz</p>
+  </div>
+  <div class="divider"></div>
+  <div class="section">
+    <h5>Almacenamiento</h5>
+    <p>4GB internos y 512MB de RAM</p>
+  </div>
+  <div class="divider"></div>
+  <div class="section">
+    <h5>Bateria</h5>
+    <p>	420 mAh</p>
+  </div>
+    </div>
+  </div>
+  </div>
+  <div class="col s4 m4">
+        <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator grande" src="img/samsung2.png">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">Samsung Gear S2<i class="material-icons right">more_vert</i></span>
+      <p><a href="#">Comprar<i class="material-icons left">shopping_cart</i></a></p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4"><Especificaciones></Especificaciones><i class="material-icons right">close</i></span>
+      <div class="divider"></div>
+  <div class="section">
+    <h5>Pantalla</h5>
+    <p>LCD de 1,6 pulgadas con resolucion de 320 x 320 píxeles</p>
+  </div>
+  <div class="divider"></div>
+  <div class="section">
+    <h5>Procesador</h5>
+    <p>Qualcomm Snapdragon 400 a 1,2 GHz</p>
+  </div>
+  <div class="divider"></div>
+  <div class="section">
+    <h5>Almacenamiento</h5>
+    <p>4GB internos y 512MB de RAM</p>
+  </div>
+  <div class="divider"></div>
+  <div class="section">
+    <h5>Bateria</h5>
+    <p>	420 mAh</p>
+  </div>
+    </div>
+  </div>
+  </div>  
+  </div>
+ </div>
+ <div class="parallax-container">
+      <div class="parallax"><img class= "mediana" src="img/parallax1.jpg"></div>
+    </div>
+<div class="container">
+        <div class="row">
+        <div class="col s4 m4">
+        <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator correcto" src="img/rolex.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">COSMOGRAPH<i class="material-icons right">more_vert</i></span>
+      <p><a href="#">Comprar<i class="material-icons left">shopping_cart</i></a></p>
+     </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">COSMOGRAPH<i class="material-icons right">close</i></span>
+      <p>Presentado en 1963, el Rolex Cosmograph Daytona nació como respuesta a las necesidades
+de los pilotos de alta competición. Gracias a su cronógrafo de gran fiabilidad y a su bisel
+con escala taquimétrica, permite al piloto medir perfectamente la velocidad media hasta 400
+kilómetros o millas por hora, según su preferencia. Un icono unido al mundo del automovilismo
+de alto rendimiento por su nombre y función.</p>
+    </div>
+  </div>
+  </div>
+  <div class="col s4 m4">
+        <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator correcto" src="img/rolex2.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">PEARLMASTER 39<i class="material-icons right">more_vert</i></span>
+      <p><a href="#">Comprar<i class="material-icons left">shopping_cart</i></a></p>
+     </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">PEARLMASTER 39<i class="material-icons right">close</i></span>
+      <p>El Rolex Pearlmaster es el reloj de alta joyería más ilustre de Rolex. Diseñado con las líneas suaves y curvas del Pearlmaster, se caracteriza por sus exquisitas esferas adornadas con engastes de diamantes, zafiros o rubíes. Disponible exclusivamente en oro amarillo, oro blanco u oro Everose de 18 quilates, creado por Rolex en su propia fundición.</p>
+    </div>
+  </div>
+  </div>
+  <div class="col s4 m4">
+        <div class="card">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator correcto" src="img/rolex3.jpg">
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">EXPLORER II<i class="material-icons right">more_vert</i></span>
+      <p><a href="#">Comprar<i class="material-icons left">shopping_cart</i></a></p>
+     </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">EXPLORER II<i class="material-icons right">close</i></span>
+      <p>El Rolex Explorer II se presentó por primera vez en 1971 como el heredero natural
+del Rolex Explorer original, pero con un carácter propio. El Explorer II, una herramienta
+robusta y altamente refinada, se convirtió rápidamente en el reloj de elección
+de espeleólogos, vulcanólogos y exploradores polares o de las selvas gracias a sus características específicas.</p>
+    </div>
+  </div>
+  </div>  
+  </div>
+ </div>
+     
+      <?php
+
+      include("inc/Footer.php");
+
+      ?>
+
+      <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+      <script type="text/javascript" src="js/inicia.js"></script>
+    </body>
+  </html>
+        
